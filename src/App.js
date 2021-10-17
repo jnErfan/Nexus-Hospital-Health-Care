51660bb1a30afa2fbb8166d85374b4a84a9b2f3f
jnErfan <j.n.erfan420@gmail.com>
2021-10-18 03:05:08 +0600

    Nabvar Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch,  Route} from 'react-router-dom';
+import NavBar from './Nexus_Hospital/Pages/Shared/NavBar/NavBar';
+import Home from './Nexus_Hospital/Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-   
+   <Router>
+     <NavBar/>
+     <Switch>
+
+       <Route exact path="/">
+          <Home/>
+       </Route>
+
+       <Route path="/home">
+          <Home/>
+       </Route>
+     </Switch>
+   </Router>
     </div>
   );
 }
--- src/Nexus_Hospital/Pages/Shared/NavBar/Navbar.js
+++ src/Nexus_Hospital/Pages/Shared/NavBar/Navbar.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./NavBar.css";
+import { Button, Container, Navbar } from 'react-bootstrap';
+import { HashLink } from 'react-router-hash-link';
+import "./NavBar.css";
+import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const NavBar = () => {
+   
+    const [scrollChainge, setSrollChainge] = useState(false)
+  
+    const onScrollHeader = () => {
+      window.scrollY >= 50 ? setSrollChainge(true) : setSrollChainge(false)
+    }
+    window.addEventListener('scroll', onScrollHeader)
+
+    const activeStyle={
+        backgroundColor: "#000",
+        color: "#fff"
+      }
+
     return (
-        <div>
-            
-        </div>
+        <Navbar className={scrollChainge ? "bg-white scrollBg" : "bg-transparent scrollOutBg"} fixed="top" bg="light" variant="light" expand="md">
+  <Container>
+   <div>
+   <Navbar.Brand href="#home"> <img width="50px" src="https://i.ibb.co/G5SHpzG/logo.png" alt="" /> </Navbar.Brand>
+   </div>
+   
+    <div>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <NavLink activeStyle={activeStyle}  className="navItem" to="/home">Home</NavLink>
+      <NavLink activeStyle={activeStyle}  className="navItem" to="/services">Services</NavLink>
+      <NavLink activeStyle={activeStyle}  className="navItem" to="/blogs">Blogs</NavLink>
+      <NavLink activeStyle={activeStyle} className="navItem" to="/about">About Us</NavLink>
+      <NavLink activeStyle={activeStyle}  className="navItem" to="/contract">Contract Us</NavLink>
+    </Navbar.Collapse>
+    </div>
+
+    <div>
+   
+   {/* <span>
+     <img className="rounded-circle me-2" width="8%" src='User Img' alt="" />
+   </span> */}
+  {/*  <span className="fw-bold pe-2 text-secondary">
+     User Name
+   </span> */}
+   {/*  {
+      user ? <Button onClick={logOut} variant=" py-0 mb-2">
+        <img width="20px" src="https://cdn-icons-png.flaticon.com/512/158/158730.png" alt="" />
+      </Button> : */}
+    
+      <HashLink to="/login#login" ><Button variant="outline-dark px-3 py-0 mb-2 rounded-pill fw-bold">LOGIN</Button></HashLink>
+      <HashLink to="/signin#signin" ><Button variant="dark px-3 py-0 mb-2 rounded-pill ms-3 fw-bold">SIGN UP</Button></HashLink>
+     
+    {/* } */}
+    </div>
+  </Container>
+
+</Navbar>
     );
 };
 
-export default Navbar;
+export default NavBar;
